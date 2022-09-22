3fd6279db2818b71b8ca820088870bb48488f4d0
Junghwan Park <jup014@ucsd.edu>
2022-09-21 19:39:06 -0700

    preload the test result files

--- pybehavior/models.py
+++ pybehavior/models.py
@@ -3,60 +3,9 @@ import logging
 import pandas as pd
 from pandas.core.frame import DataFrame
 
-class Participant:
-    def __init__(self):
-        print("Participant is constructed")
-
-
 class DataSet:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.data = data
     
-    def group_by_sum(self, by=datetime.timedelta(hours=1)) -> 'DataSet':
-        participant_list = self.data['participant'].unique()
-        
-        headers = ['participant', 'start_time_local', 'end_time_local', 'value', 'timespan']
-        new_data = []
 
-        for each_participant in participant_list:
-            each_participant_data = self.data[self.data['participant']==each_participant]
-
-            each_participant_data.sort_values(by=['start_time_local', 'end_time_local'])
-            
-            first_start_time = each_participant_data.iloc[0].start_time_local
-            first_day_midnight = datetime.datetime(first_start_time.year, first_start_time.month, first_start_time.day)
-
-            last_end_time = each_participant_data.iloc[-1].end_time_local
-            last_day_midnight = datetime.datetime(last_end_time.year, last_end_time.month, last_end_time.day) + datetime.timedelta(days=1)
-
-            current_start_time = first_day_midnight
-            current_end_time = current_start_time + by
-
-            cumulative_value = 0
-
-            for index, each_row in each_participant_data.iterrows():
-                while current_end_time <= each_row['start_time_local']:
-                    new_data.append([
-                        each_participant, current_start_time, current_end_time, cumulative_value, by
-                    ])
-                    current_start_time += by
-                    current_end_time += by
-                    cumulative_value = 0
-
-                if each_row['start_time_local'] < current_end_time:
-                    cumulative_value += each_row['value']
-
-            while current_end_time <= last_day_midnight:
-                new_data.append([
-                        each_participant, current_start_time, current_end_time, cumulative_value, by
-                    ])
-                current_start_time += by
-                current_end_time += by
-                cumulative_value = 0
-
-        new_dataframe = pd.DataFrame(new_data, columns=headers)
-        new_dataset = DataSet()
-        new_dataset.data = new_dataframe
-
-        return new_dataset
             
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import logging
 import datetime
@@ -14,34 +15,70 @@ class ParticipantTestCase(TestCase):
         logging.debug('Starting Test: {}'.format(datetime.datetime.now()))
         pd.set_option('display.max_colwidth', None)
         pd.set_option('display.max_columns', None)  
-        self.data = pd.read_csv('data/jawbone.csv', low_memory=False)
+
+    def try_to_load_data(self, data_id:int, src_data=None):
+        data_dir = 'data/'
+        data_path = os.path.join(data_dir, "data{}.pickle".format(data_id))
+
+        if os.path.exists(data_path):
+            return pd.read_pickle(data_path)
+        else:
+            if data_id == 1:
+                data = pd.read_csv('data/jawbone.csv', low_memory=False)
+            elif data_id == 2:
+                data = Loader.pick_columns(data=src_data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
+            elif data_id == 3:
+                data = src_data.query('steps >= 60')
+            elif data_id == 4:
+                data = Preprocessor.merge_rows(src_data)
+            elif data_id == 5:
+                data = src_data.query('duration >= datetime.timedelta(minutes=5)')
+            elif data_id == 6:
+                data = Preprocessor.get_hourly_activity_data(src_data)
+            else:
+                raise Exception("Unknown data_id: {}".format(data_id))
+            data.to_pickle(data_path)
+            return data
 
     def test_pick_columns(self):
-        data2 = Loader.pick_columns(data=self.data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
     
     def test_filter_out_1(self):
-        data2 = Loader.pick_columns(data=self.data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
-        data3 = data2.query('steps >= 60')
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
+        data3 = self.try_to_load_data(3, data2)
 
     def test_merge_rows(self):
-        data2 = Loader.pick_columns(data=self.data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
-        data3 = data2.query('steps >= 60')
-        data4 = Preprocessor.merge_rows(data3)
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
+        data3 = self.try_to_load_data(3, data2)
+        data4 = self.try_to_load_data(4, data3)
     
     def test_filter_out_2(self):
-        data2 = Loader.pick_columns(data=self.data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
-        data3 = data2.query('steps >= 60')
-        data4 = Preprocessor.merge_rows(data3)
-        data5 = data4.query('duration >= datetime.timedelta(minutes=5)')
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
+        data3 = self.try_to_load_data(3, data2)
+        data4 = self.try_to_load_data(4, data3)
+        data5 = self.try_to_load_data(5, data4)
 
     def test_sample(self):
-        data2 = Loader.pick_columns(data=self.data, user='user', start_datetime='start_utime_local', end_datetime='end_utime_local', values=['steps'])
-        data3 = data2.query('steps >= 60')
-        data4 = Preprocessor.merge_rows(data3)
-        data5 = data4.query('duration >= datetime.timedelta(minutes=5)')
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
+        data3 = self.try_to_load_data(3, data2)
+        data4 = self.try_to_load_data(4, data3)
+        data5 = self.try_to_load_data(5, data4)
+        data6 = self.try_to_load_data(6, data5)
 
-        hours_with_012 = Preprocessor.get_hourly_activity_data(data5)
-        hours_with_012.to_csv('hours_with_012.csv')
+    def test_query(self):
+        data = self.try_to_load_data(1)
+        data2 = self.try_to_load_data(2, data)
+        data3 = self.try_to_load_data(3, data2)
+        data4 = self.try_to_load_data(4, data3)
+        data5 = self.try_to_load_data(5, data4)
+        data6 = self.try_to_load_data(6, data5)
+
+
 
 
 if __name__ == '__main__':
