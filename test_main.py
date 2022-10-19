34a40b9caa26590794c1a027e43b81a370415f8c
Junghwan Park <jup014@ucsd.edu>
2022-10-18 21:33:05 -0700

    resume feature, memory management

--- test_main.py
+++ test_main.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import BayesLDM.BayesLDM as BayesLDM
 from pybehavior.tools import MCMC_Parser, BayesModelBuilder
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 
 relations = [
@@ -33,7 +33,7 @@ rnum_list = [4, 5, 6, 8, 9, 11, 17, 18, 22, 25, 26, 27, 30, 31, 33, 34, 42, 43, 
 db = TinyDB('data/ema/db.json')
 
 for rnum in rnum_list:
-    data = data_original.loc[data_original.rnum == rnum].reset_index(drop=True)
+    data = data_original.loc[data_original.rnum == rnum].reset_index(drop=True).copy()
     data = data[columns]
 
     # ['rnum', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'E11', 'E12.1.1',
@@ -45,56 +45,64 @@ for rnum in rnum_list:
 
     data.name = "data"
 
-    builder = BayesModelBuilder('model')
-
-
-    builder.add_variable_regression_edge(relations)
-
+    
     N = data.shape[0]
 
     for window_size in range(10, N):
         actual_window_size = window_size + 1        # including the last missing data
         for start_point in range(0, N - actual_window_size - 1):
-            print("cropping {}:{}".format(start_point, actual_window_size + start_point))
-            current_data = data.loc[start_point:(start_point + window_size)].reset_index(drop=True).copy()
-            original_data = current_data.loc[window_size:window_size].copy()
-            if original_data.isna().sum(axis=1).to_list()[0] > 0:
-                db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
-                db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
+            Log = Query()
+            logs = db.search((Log.rnum == rnum) & (Log.method == 'Sustain') & (Log.window_size == window_size) & (Log.start_point == start_point))
+            if len(logs) == 0:
+                print("cropping {}:{}".format(start_point, actual_window_size + start_point))
+                current_data = data.loc[start_point:(start_point + window_size)].reset_index(drop=True).copy()
+                original_data = current_data.loc[window_size:window_size].copy()
+                if original_data.isna().sum(axis=1).to_list()[0] > 0:
+                    db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
+                    db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
+                else:
+                    builder = BayesModelBuilder('model')
+                    builder.add_variable_regression_edge(relations)
+                    # bayes LDM method
+                    current_data.loc[window_size:window_size, columns] = None  # type: ignore
+                    current_data = current_data.assign(t=range(0, actual_window_size))
+                    builder.set_index('t', 0, window_size)
+                    current_data.name = 'dataset'
+                    model = BayesLDM.compile(builder.get_full_model(), obs=columns, data=[current_data]) 
+                    samples = model.sample(b_post_process=True)
+                    parser = MCMC_Parser(model)
+
+                    distance = 0
+                    estimated = []
+                    
+                    for each_column in columns2:
+                        each_estimated = int(parser.variable[each_column][window_size])
+                        estimated.append(each_estimated)
+                        distance += pow(original_data.iloc[0][each_column] - each_estimated, 2)
+                    
+                    distance = math.sqrt(distance) / len(columns2)  # type: ignore
+
+                    db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
+
+                    # sustain method
+                    distance = 0
+                    estimated = []
+                    for each_column in columns2:
+                        a_col = current_data[each_column].to_list()
+                        for i in range(window_size - 1, 0, -1):
+                            each_estimated = a_col[i]
+                            if not np.isnan(each_estimated):
+                                break
+                        estimated.append(each_estimated)
+                        distance += pow(original_data.iloc[0][each_column] - each_estimated, 2)
+                    
+                    distance = math.sqrt(distance) / len(columns2)  # type: ignore
+
+                    db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
+
+                    del model, parser, samples, builder
+                del current_data, original_data
             else:
-                # bayes LDM method
-                current_data.loc[window_size:window_size, columns] = None  # type: ignore
-                current_data = current_data.assign(t=range(0, actual_window_size))
-                builder.set_index('t', 0, window_size)
-                current_data.name = 'dataset'
-                model = BayesLDM.compile(builder.get_full_model(), obs=columns, data=[current_data]) 
-                samples = model.sample(b_post_process=True)
-                parser = MCMC_Parser(model)
-
-                distance = 0
-                estimated = []
-                
-                for each_column in columns2:
-                    each_estimated = int(parser.variable[each_column][window_size])
-                    estimated.append(each_estimated)
-                    distance += pow(original_data.iloc[0][each_column] - each_estimated, 2)
-                
-                distance = math.sqrt(distance) / len(columns2)  # type: ignore
-
-                db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
-
-                # sustain method
-                distance = 0
-                estimated = []
-                for each_column in columns2:
-                    a_col = current_data[each_column].to_list()
-                    for i in range(window_size - 1, 0, -1):
-                        each_estimated = a_col[i]
-                        if not np.isnan(each_estimated):
-                            break
-                    estimated.append(each_estimated)
-                    distance += pow(original_data.iloc[0][each_column] - each_estimated, 2)
-                
-                distance = math.sqrt(distance) / len(columns2)  # type: ignore
-
-                db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
+                pass
+            del logs, Log
+    del data
