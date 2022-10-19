fcadc5ae7359e900ec50fce5128367ddc8f2439c
Junghwan Park <jup014@ucsd.edu>
2022-10-18 22:34:57 -0700

    transitioned to mongodb

--- test_main.py
+++ test_main.py
@@ -9,8 +9,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import BayesLDM.BayesLDM as BayesLDM
 from pybehavior.tools import MCMC_Parser, BayesModelBuilder
-from tinydb import TinyDB, Query
-
 
 relations = [
     ('self_efficacy', 'self_efficacy'),
@@ -31,43 +29,41 @@ data_original = data_original.rename(columns={'steps': 'behavior', 'cue_to_actio
 
 rnum_list = [4, 5, 6, 8, 9, 11, 17, 18, 22, 25, 26, 27, 30, 31, 33, 34, 42, 43, 44, 47, 48]
 
-db = TinyDB('data/ema/db.json')
+import pymongo
 
-if not os.path.exists('data/ema/queue.json'):
-    queue = TinyDB('data/ema/queue.json')
-    for rnum in rnum_list:
-        data = data_original.loc[data_original.rnum == rnum].reset_index(drop=True).copy()
-        data = data[columns]
+client = pymongo.MongoClient("mongodb+srv://yourmove_server:{}@cluster0.exk1ym6.mongodb.net/?retryWrites=true&w=majority".format(os.environ.get('MONGODB_PASSWORD')))
+db = client.bayesldm.db
+queue = client.bayesldm.queue
 
-        data.name = "data"
-        
-        N = data.shape[0]
+## for the first time
+# save_queue = []
+# for rnum in rnum_list:
+#     data = data_original.loc[data_original.rnum == rnum].reset_index(drop=True).copy()
+#     data = data[columns]
 
-        save_queue = []
-        for window_size in range(10, N):
-            actual_window_size = window_size + 1        # including the last missing data
-            for start_point in range(0, N - actual_window_size - 1):
-                save_queue.append({'rnum': rnum, 'window_size': window_size, 'start_point': start_point, 'status': 'queued'})
-                if len(save_queue) == 1000:
-                    queue.insert_multiple(save_queue)
-                    del save_queue
-                    save_queue = []
-        del data
+#     data.name = "data"
+    
+#     N = data.shape[0]
 
-        queue.insert_multiple(save_queue)
-        del save_queue
-        save_queue = []
+#     for window_size in range(10, N):
+#         actual_window_size = window_size + 1        # including the last missing data
+#         for start_point in range(0, N - actual_window_size - 1):
+#             save_queue.append({'rnum': rnum, 'window_size': window_size, 'start_point': start_point, 'status': 'queued'})
+#             if len(save_queue) == 1000:
+#                 queue.insert_many(save_queue)
+#                 del save_queue
+#                 save_queue = []
+#     del data
 
-queue = TinyDB('data/ema/queue.json')
+# queue.insert_many(save_queue)
+# del save_queue
+# save_queue = []
+
 while True:
-    Job = Query()
-
-    job = queue.get(Job.status == 'queued')
+    job = queue.find_one_and_update({'status': 'queued'}, {'$set': {'status': 'fetched'}})
     if job is None:
         break
     
-    queue.update({'status': 'fetched'}, (Job.rnum == job['rnum']) & (Job.window_size == job['window_size']) & (Job.start_point == job['start_point']))
-
     rnum = job['rnum']
     window_size = job['window_size']
     start_point = job['start_point']
@@ -81,15 +77,14 @@ while True:
 
     actual_window_size = window_size + 1        # including the last missing data
     
-    Log = Query()
-    log_count = db.count((Log.rnum == rnum) & (Log.method == 'Sustain') & (Log.window_size == window_size) & (Log.start_point == start_point))
+    log_count = db.count_documents({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'start_point': start_point})
     if log_count == 0:
         print("cropping {}:{}".format(start_point, actual_window_size + start_point))
         current_data = data.loc[start_point:(start_point + window_size)].reset_index(drop=True).copy()
         original_data = current_data.loc[window_size:window_size].copy()
         if original_data.isna().sum(axis=1).to_list()[0] > 0:
-            db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
-            db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
+            db.insert_one({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
+            db.insert_one({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns, 'start_point': start_point, 'distance': None, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': None})
         else:
             builder = BayesModelBuilder('model')
             builder.add_variable_regression_edge(relations)
@@ -112,7 +107,7 @@ while True:
             
             distance = math.sqrt(distance) / len(columns2)  # type: ignore
 
-            db.insert({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
+            db.insert_one({'rnum': rnum, 'method': 'BayesLDM', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
 
             # sustain method
             distance = 0
@@ -128,10 +123,10 @@ while True:
             
             distance = math.sqrt(distance) / len(columns2)  # type: ignore
 
-            db.insert({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
+            db.insert_one({'rnum': rnum, 'method': 'Sustain', 'window_size': window_size, 'columns': columns2, 'start_point': start_point, 'distance': None if distance is np.NaN else distance, 'original_data': original_data[columns].iloc[0].to_list(), 'estimated': estimated})
 
             del model, parser, samples, builder
         del current_data, original_data
     else:
         pass
-    del Log, data
+    del data
